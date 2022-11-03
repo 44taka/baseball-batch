540a9f22e019bf5f557f5a67b53348b49173ecf2
44taka <yoshitaka.i.v.x.jade@gmail.com>
2022-11-03 10:05:02 +0900

    add team color code

--- app/database/migrations/2022_09_26_120001_create_team_mst.py
+++ app/database/migrations/2022_09_26_120001_create_team_mst.py
@@ -11,6 +11,7 @@ class CreateTeamMst(Migration):
             table.increments('id')
             table.small_integer('league_kbn')
             table.string('team_name', 50)
+            table.string('team_color_cd', 10)
             table.integer('yahoo_team_id').unsigned()
             table.boolean('is_deleted').default(0)
             table.timestamps()
