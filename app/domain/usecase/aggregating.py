62f92b19bb2af353d69cc138b11d2016be709344
44taka <yoshitaka.i.v.x.jade@gmail.com>
2023-01-30 15:29:56 +0900

    不要なファイルを削除

--- app/domain/usecase/aggregating.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-import datetime
-
-
-class IAggregatingUseCase(ABC):
-    @abstractmethod
-    def aggregate(self, date: datetime.date) -> int:
-        raise NotImplementedError
--- app/domain/usecase/test.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class ITestUseCase(ABC):
-    @abstractmethod
-    def find_all(self):
-        raise NotImplementedError
